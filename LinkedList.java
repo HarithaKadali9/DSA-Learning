46c6e6c8aa4fd02fce4d9b9f2b1d038dfcc0638d
HarithaKadali9 <kadaliharitha23@gmail.com>
2025-05-18 09:55:26 +0530

    Inserting operations

--- LinkedList.java
+++ LinkedList.java
@@ -12,6 +12,7 @@ class Node{
 }
 public class LinkedList{
     private static Node convertArrToList(int[] arr){
+        if(arr.length==0) return null;
         Node head=new Node(arr[0]);
         Node temp=head;
         for(int i=1;i<arr.length;i++){
@@ -26,6 +27,14 @@ public class LinkedList{
             System.out.print(head.data+" ");
             head=head.next;
         }
+    }
+    private static int length(Node head){
+        int count=0;
+        while(head!=null){
+            head=head.next;
+            count++;
+        }
+        return count;
     }
     private static Node deleteNode(Node head){
         if(head==null || head.next==null) return null;
@@ -83,11 +92,77 @@ public class LinkedList{
         prev.next=prev.next.next;
         return head;
     }
+    private static Node insertAtHead(Node head, int val){
+        if(head==null){
+            return new Node(val);
+        }
+        Node newNode=new Node(val, head);
+        //newNode.next=head;
+        head=newNode;
+        return head;
+    }
+    private static Node insertAtTail(Node head, int val){
+        if(head==null){
+            return new Node(val);
+        }
+        Node temp=head;
+        while(temp.next!=null) temp=temp.next;
+        Node newNode=new Node(val, null);
+        temp.next=newNode;
+        return head;
+    }
+    private static Node insertAtK(Node head, int val, int k){
+        if(head==null){
+            if(k==1) return new Node(val);
+        }
+        if(k>length(head)) {
+            System.out.println("Please enter k below the number of nodes");
+            return null;
+        }
+        if(k==1) return insertAtHead(head, val);
+        int count=0;
+        Node temp=head;
+        while(temp.next!=null){
+            count++;
+            if(count==k-1)break;
+            temp=temp.next;
+        }
+        Node newNode=new Node(val, temp.next);
+        temp.next=newNode;
+        return head;
+    }
+    private static Node insertBeforeValue(Node head, int val, int ele){
+        if(head==null){
+            System.out.println("It is not existing because if u want to insert before value then u must have to have a node");
+            return null;
+        }
+        if(head.data==ele){
+            return insertAtHead(head, val);
+        }
+        Node temp=head;
+        Node prev=null;
+        boolean flag=false;
+        while(temp.next!=null){
+            if(temp.data==ele){
+                flag=true;
+                break;
+            }
+            prev=temp;
+            temp=temp.next;
+        }
+        if(!flag){
+            System.out.println("Element not found But i am inserting at tail");
+            return insertAtTail(head, val);
+        }
+        Node newNode=new Node(val, temp);
+        prev.next=newNode;
+        return head;
+    }
     public static void main(String[] arsgs){
-        int[] arr={12};
+        int[] arr={12, 5, 8, 7};
         Node head;
         head=convertArrToList(arr);
-        head=deleteNodeByValue(head,12);
+        head=insertBeforeValue(head,100, 4);
         print(head);
     }
 }
